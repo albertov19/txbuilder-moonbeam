1c15a95b15385258aa41636ecdab60a8b653f181
Albertov19 <albertoviera@gmail.com>
2023-02-27 18:06:34 +0100

    added many staking options

--- components/staking.tsx
+++ components/staking.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Input, Button, Label, Form, Message } from 'semantic-ui-react';
+import { Container, Input, Button, Label, Form, Message, Checkbox, GridColumn, Grid } from 'semantic-ui-react';
 import { subProvider } from '../web3/api';
 import * as ethers from 'ethers';
 
 const StakingBuilder = ({ network }) => {
+  const [stkOption, setStkOption] = useState('stake');
   const [stkAddress, setStkAddress] = useState('');
   const [colAddress, setColAddress] = useState('');
   const [amount, setAmount] = useState(BigInt(0));
   const [autoCompound, setAutoCompound] = useState(BigInt(0));
   const [stakingCall, setStakingCall] = useState({
     targetNetwork: '',
+    stkOption: '',
     stkAddress: '',
     colAddress: '',
     amount: BigInt(0),
@@ -38,6 +40,20 @@ const StakingBuilder = ({ network }) => {
     setTokenLabel(network.token);
     setURL(network.url);
 
+    if (stkOption == 'stake') {
+      await calculateStake(api);
+    } else if (stkOption == 'increase') {
+      await calculateIncrease(api);
+    } else if (stkOption == 'decrease') {
+      await calculateDecrease(api);
+    } else if (stkOption == 'revoke') {
+      await calculateRevoke(api);
+    } else if (stkOption == 'execute') {
+      await calculateExecute(api);
+    }
+  };
+
+  const calculateStake = async (api) => {
     //Check Input
     // Staking Address
     let check = ethers.utils.isAddress(stkAddress);
@@ -127,6 +143,269 @@ const StakingBuilder = ({ network }) => {
     // Set Calculated Data
     setStakingCall({
       targetNetwork: network.key,
+      stkOption,
+      colAddress,
+      stkAddress,
+      amount,
+      autoCompound,
+      candidateDelegationCount,
+      candidateAutoCompoundingDelegationCount,
+      delegationCount,
+      tokenLabel,
+    });
+  };
+
+  const calculateIncrease = async (api) => {
+    let autoCompound = BigInt(0);
+    let candidateDelegationCount = BigInt(0);
+    let candidateAutoCompoundingDelegationCount = BigInt(0);
+    let delegationCount = BigInt(0);
+
+    // Staking Address
+    let check = ethers.utils.isAddress(stkAddress);
+    if (!check) {
+      setErrorMessage('Address with funds not valid!');
+      return;
+    }
+
+    //Check Input
+    // Collator Address
+    check = ethers.utils.isAddress(colAddress);
+    if (!check) {
+      setErrorMessage('Collator address not valid!');
+      return;
+    }
+
+    // Balance
+    let balance = ((await api.query.system.account(stkAddress)) as any).toHuman().data;
+    check =
+      BigInt(balance.free.replaceAll(',', '')) -
+        BigInt(balance.miscFrozen.replaceAll(',', '')) -
+        BigInt(100000000000000000) >
+      BigInt(amount);
+    if (!check) {
+      setErrorMessage('Not enough balance!');
+      return;
+    }
+
+    // Check if you are Staking to that collator
+    const candidatesStaked = ((await api.query.parachainStaking.delegatorState(stkAddress)) as any).toHuman();
+    let isStaked;
+    candidatesStaked.delegations.map((candidate) => {
+      if (colAddress === candidate.owner) {
+        isStaked = true;
+      }
+    });
+    if (!isStaked) {
+      setErrorMessage("You can't increase to a collator you are not staking!");
+      return;
+    }
+
+    // Create Staking Call
+    const stakingCall = await api.tx.parachainStaking.delegatorBondMore(colAddress, amount);
+
+    setStakingCallData(stakingCall.method.toHex());
+
+    // Generate Proxy call
+    const proxyCall = await api.tx.proxy.proxy(stkAddress, null, stakingCall);
+
+    setProxyCallData(proxyCall.method.toHex());
+
+    // Set Calculated Data
+    setStakingCall({
+      targetNetwork: network.key,
+      stkOption,
+      colAddress,
+      stkAddress,
+      amount,
+      autoCompound,
+      candidateDelegationCount,
+      candidateAutoCompoundingDelegationCount,
+      delegationCount,
+      tokenLabel,
+    });
+  };
+
+  const calculateDecrease = async (api) => {
+    let autoCompound = BigInt(0);
+    let candidateDelegationCount = BigInt(0);
+    let candidateAutoCompoundingDelegationCount = BigInt(0);
+    let delegationCount = BigInt(0);
+
+    // Staking Address
+    let check = ethers.utils.isAddress(stkAddress);
+    if (!check) {
+      setErrorMessage('Address with funds not valid!');
+      return;
+    }
+
+    //Check Input
+    // Collator Address
+    check = ethers.utils.isAddress(colAddress);
+    if (!check) {
+      setErrorMessage('Collator address not valid!');
+      return;
+    }
+
+    // Balance
+    let balance = ((await api.query.system.account(stkAddress)) as any).toHuman().data;
+    check =
+      BigInt(balance.free.replaceAll(',', '')) -
+        BigInt(balance.miscFrozen.replaceAll(',', '')) -
+        BigInt(100000000000000000) >
+      BigInt(amount);
+    if (!check) {
+      setErrorMessage('Not enough balance!');
+      return;
+    }
+
+    // Check if you are Staking to that collator
+    const candidatesStaked = ((await api.query.parachainStaking.delegatorState(stkAddress)) as any).toHuman();
+    let isStaked;
+    candidatesStaked.delegations.map((candidate) => {
+      if (colAddress === candidate.owner) {
+        isStaked = true;
+      }
+    });
+    if (!isStaked) {
+      setErrorMessage("You can't decrease to a collator you are not staking!");
+      return;
+    }
+
+    // Create Staking Call
+    const stakingCall = await api.tx.parachainStaking.scheduleDelegatorBondLess(colAddress, amount);
+
+    setStakingCallData(stakingCall.method.toHex());
+
+    // Generate Proxy call
+    const proxyCall = await api.tx.proxy.proxy(stkAddress, null, stakingCall);
+
+    setProxyCallData(proxyCall.method.toHex());
+
+    // Set Calculated Data
+    setStakingCall({
+      targetNetwork: network.key,
+      stkOption,
+      colAddress,
+      stkAddress,
+      amount,
+      autoCompound,
+      candidateDelegationCount,
+      candidateAutoCompoundingDelegationCount,
+      delegationCount,
+      tokenLabel,
+    });
+  };
+
+  const calculateRevoke = async (api) => {
+    let autoCompound = BigInt(0);
+    let candidateDelegationCount = BigInt(0);
+    let candidateAutoCompoundingDelegationCount = BigInt(0);
+    let delegationCount = BigInt(0);
+    let amount = BigInt(0);
+
+    // Staking Address
+    let check = ethers.utils.isAddress(stkAddress);
+    if (!check) {
+      setErrorMessage('Address with funds not valid!');
+      return;
+    }
+
+    //Check Input
+    // Collator Address
+    check = ethers.utils.isAddress(colAddress);
+    if (!check) {
+      setErrorMessage('Collator address not valid!');
+      return;
+    }
+
+    // Check if you are Staking to that collator
+    const candidatesStaked = ((await api.query.parachainStaking.delegatorState(stkAddress)) as any).toHuman();
+    let isStaked;
+    candidatesStaked.delegations.map((candidate) => {
+      if (colAddress === candidate.owner) {
+        isStaked = true;
+      }
+    });
+    if (!isStaked) {
+      setErrorMessage("You can't revoke a collator you are not staking!");
+      return;
+    }
+
+    // Create Staking Call
+    const stakingCall = await api.tx.parachainStaking.scheduleRevokeDelegation(colAddress);
+
+    setStakingCallData(stakingCall.method.toHex());
+
+    // Generate Proxy call
+    const proxyCall = await api.tx.proxy.proxy(stkAddress, null, stakingCall);
+
+    setProxyCallData(proxyCall.method.toHex());
+
+    // Set Calculated Data
+    setStakingCall({
+      targetNetwork: network.key,
+      stkOption,
+      colAddress,
+      stkAddress,
+      amount,
+      autoCompound,
+      candidateDelegationCount,
+      candidateAutoCompoundingDelegationCount,
+      delegationCount,
+      tokenLabel,
+    });
+  };
+
+  const calculateExecute = async (api) => {
+    let autoCompound = BigInt(0);
+    let candidateDelegationCount = BigInt(0);
+    let candidateAutoCompoundingDelegationCount = BigInt(0);
+    let delegationCount = BigInt(0);
+    let amount = BigInt(0);
+
+    // Staking Address
+    let check = ethers.utils.isAddress(stkAddress);
+    if (!check) {
+      setErrorMessage('Address with funds not valid!');
+      return;
+    }
+
+    //Check Input
+    // Collator Address
+    check = ethers.utils.isAddress(colAddress);
+    if (!check) {
+      setErrorMessage('Collator address not valid!');
+      return;
+    }
+
+    // Check if you are Staking to that collator
+    const candidatesStaked = ((await api.query.parachainStaking.delegatorState(stkAddress)) as any).toHuman();
+    let isStaked;
+    candidatesStaked.delegations.map((candidate) => {
+      if (colAddress === candidate.owner) {
+        isStaked = true;
+      }
+    });
+    if (!isStaked) {
+      setErrorMessage("You can't revoke a collator you are not staking!");
+      return;
+    }
+
+    // Create Staking Call
+    const stakingCall = await api.tx.parachainStaking.executeDelegationRequest(stkAddress, colAddress);
+
+    setStakingCallData(stakingCall.method.toHex());
+
+    // Generate Proxy call
+    const proxyCall = await api.tx.proxy.proxy(stkAddress, null, stakingCall);
+
+    setProxyCallData(proxyCall.method.toHex());
+
+    // Set Calculated Data
+    setStakingCall({
+      targetNetwork: network.key,
+      stkOption,
       colAddress,
       stkAddress,
       amount,
@@ -146,10 +425,9 @@ const StakingBuilder = ({ network }) => {
     }
   };
 
-  return (
-    <Container>
-      <div style={{ width: '50%' }}>
-        <h3>Staking Transaction</h3>
+  const stakingForm = () => {
+    return (
+      <div className='stakingInput'>
         <Input
           fluid
           label={{ content: 'Account with Funds:' }}
@@ -202,9 +480,248 @@ const StakingBuilder = ({ network }) => {
           <Label>%</Label>
         </Input>
       </div>
+    );
+  };
+
+  const increaseForm = () => {
+    return (
+      <div className='increaseInput'>
+        <Input
+          fluid
+          label={{ content: 'Account with Funds:' }}
+          placeholder='Address with the tokens...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setStkAddress(address);
+          }}
+        />
+        <br />
+        <Input
+          fluid
+          label={{ content: 'Enter Collator Address:' }}
+          placeholder='Collator you want increase stake...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setColAddress(address);
+          }}
+        />
+        <br />
+
+        <Input
+          fluid
+          labelPosition='right'
+          type='text'
+          placeholder='Amount of tokens to increase...'
+          onChange={(input) => {
+            let amount;
+            if (input.target.value) {
+              amount = ethers.utils.parseEther(input.target.value.toString()).toString();
+            }
+            setAmount(amount);
+          }}
+        >
+          <Label>Enter Increase Amount:</Label>
+          <input />
+          <Label>{tokenLabel}</Label>
+        </Input>
+        <br />
+      </div>
+    );
+  };
+
+  const decreaseForm = () => {
+    return (
+      <div className='decreaseInput'>
+        <Input
+          fluid
+          label={{ content: 'Account with Funds:' }}
+          placeholder='Address with the tokens...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setStkAddress(address);
+          }}
+        />
+        <br />
+        <Input
+          fluid
+          label={{ content: 'Enter Collator Address:' }}
+          placeholder='Collator you want to decrease stake...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setColAddress(address);
+          }}
+        />
+        <br />
+
+        <Input
+          fluid
+          labelPosition='right'
+          type='text'
+          placeholder='Amount of tokens to decrease...'
+          onChange={(input) => {
+            let amount;
+            if (input.target.value) {
+              amount = ethers.utils.parseEther(input.target.value.toString()).toString();
+            }
+            setAmount(amount);
+          }}
+        >
+          <Label>Enter Decrease Amount:</Label>
+          <input />
+          <Label>{tokenLabel}</Label>
+        </Input>
+        <br />
+      </div>
+    );
+  };
+
+  const revokeForm = () => {
+    return (
+      <div className='revokeInput'>
+        <Input
+          fluid
+          label={{ content: 'Account with Funds:' }}
+          placeholder='Address with the tokens...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setStkAddress(address);
+          }}
+        />
+        <br />
+        <Input
+          fluid
+          label={{ content: 'Enter Collator Address:' }}
+          placeholder='Collator you want to revoke stake...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setColAddress(address);
+          }}
+        />
+        <br />
+      </div>
+    );
+  };
+
+  const executeForm = () => {
+    return (
+      <div className='executeInput'>
+        <Input
+          fluid
+          label={{ content: 'Account with Funds:' }}
+          placeholder='Address with the tokens...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setStkAddress(address);
+          }}
+        />
+        <br />
+        <Input
+          fluid
+          label={{ content: 'Enter Collator Address:' }}
+          placeholder='Collator you want to execute request from...'
+          onChange={(input) => {
+            let address = checkAddress(input.target.value);
+            setColAddress(address);
+          }}
+        />
+        <br />
+      </div>
+    );
+  };
+
+  return (
+    <Container>
+      <div style={{ width: '50%' }}>
+        <h3>Staking Transaction</h3>
+        <Form>
+          <Grid>
+            <GridColumn width={3}>
+              <Form.Field>
+                <Checkbox
+                  radio
+                  label='Stake'
+                  name='checkboxRadioGroup'
+                  value='stake'
+                  checked={stkOption === 'stake'}
+                  onChange={(e, data) => {
+                    setStkOption(data.value);
+                    console.log(stkAddress);
+                    console.log(colAddress);
+                    console.log(amount);
+                    console.log(autoCompound);
+                  }}
+                />
+              </Form.Field>
+            </GridColumn>
+            <GridColumn width={3}>
+              <Form.Field>
+                <Checkbox
+                  radio
+                  label='Increase'
+                  name='checkboxRadioGroup'
+                  value='increase'
+                  checked={stkOption === 'increase'}
+                  onChange={(e, data) => {
+                    setStkOption(data.value);
+                    setAutoCompound(BigInt(0));
+                  }}
+                />
+              </Form.Field>
+            </GridColumn>
+            <GridColumn width={3}>
+              <Form.Field>
+                <Checkbox
+                  radio
+                  label='Decrease'
+                  name='checkboxRadioGroup'
+                  value='decrease'
+                  checked={stkOption === 'decrease'}
+                  onChange={(e, data) => setStkOption(data.value)}
+                />
+              </Form.Field>
+            </GridColumn>
+            <GridColumn width={3}>
+              <Form.Field>
+                <Checkbox
+                  radio
+                  label='Revoke'
+                  name='checkboxRadioGroup'
+                  value='revoke'
+                  checked={stkOption === 'revoke'}
+                  onChange={(e, data) => setStkOption(data.value)}
+                />
+              </Form.Field>
+            </GridColumn>
+            <GridColumn width={3}>
+              <Form.Field>
+                <Checkbox
+                  radio
+                  label='Execute'
+                  name='checkboxRadioGroup'
+                  value='execute'
+                  checked={stkOption === 'execute'}
+                  onChange={(e, data) => setStkOption(data.value)}
+                />
+              </Form.Field>
+            </GridColumn>
+          </Grid>
+        </Form>
+        <br />
+        {stkOption == 'stake'
+          ? stakingForm()
+          : stkOption == 'increase'
+          ? increaseForm()
+          : stkOption == 'decrease'
+          ? decreaseForm()
+          : stkOption == 'revoke'
+          ? revokeForm()
+          : stkOption == 'execute'
+          ? executeForm()
+          : ''}
+      </div>
       <br />
       <Form onSubmit={() => calculate()} error={!!errorMessage}>
-        <Button type='submit' disabled={!stkAddress || !colAddress || !amount || !autoCompound} color='orange'>
+        <Button type='submit' color='orange'>
           Calculate Data
         </Button>
         <Message style={{ width: '50%' }} error header='Oops!' content={errorMessage} />
@@ -226,6 +743,7 @@ const StakingBuilder = ({ network }) => {
           <br />
           <ul>
             <li>{stakingCall.targetNetwork}</li>
+            <li>Staking Action: {stakingCall.stkOption.toUpperCase()}</li>
             <li>Account with Funds: {stakingCall.stkAddress}</li>
             <li>Collator: {stakingCall.colAddress} </li>
             <li>
